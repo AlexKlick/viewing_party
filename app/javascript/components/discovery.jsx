c11bb27bd501edddfbb774b699ad9644df65bd4c
Alex Klick <aklick12@gmail.com>
2021-08-29 12:51:08 -0600

    got react working, setup basic discover page

---
+++ app/javascript/components/discovery.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Discovery extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Greeting: {this.props.greeting}
+      </React.Fragment>
+    );
+  }
+}
+export default Discovery
