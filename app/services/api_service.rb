352646afecfe2e54efe96efd5268eb2d61383dee
Alex Klick <aklick12@gmail.com>
2021-08-31 20:08:02 -0600

    flash messages now use bootstrap classes for styling

--- app/services/api_service.rb
+++ app/services/api_service.rb
@@ -1,10 +1,5 @@
 class ApiService
-  # def get_data(endpoint)
-  #   response = Faraday.get(endpoint)
-  #   data = response.body
-  #   JSON.parse(data, symbolize_names: true)
-  # end
-
+  
   def get_data(endpoint)
     Faraday.new(endpoint)
   end
